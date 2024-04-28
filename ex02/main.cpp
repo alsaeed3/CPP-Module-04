342ef640f4aa199e6e52a7d46fffd6e58871f5dc
alsaeed3 <ali.eissa.570@gmail.com>
2024-04-28 17:02:22 +0400

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: alsaeed <alsaeed@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:15:23 by alsaeed           #+#    #+#             */
-/*   Updated: 2024/04/28 16:54:31 by alsaeed          ###   ########.fr       */
+/*   Updated: 2024/04/28 17:02:17 by alsaeed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include "Dog.hpp"
 
 int main() {
+
+	/* Instantiate an object of the abstract class will cause a compilation error */
     AAnimal a;
 
     return 0;
